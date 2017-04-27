805b1f4ae967dcc44708a4ed3c0b18b80aa9fded
maximebl <maximebl@videotron.ca>
2017-04-27 13:32:16 -0400

    Basic Flat button implementation in the top menu

--- src/components/TopMenu/TopMenu.js
+++ src/components/TopMenu/TopMenu.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import './TopMenu.css';
+import TopMenuButton from './TopMenuButton';
 
 class TopMenu extends React.Component {
     render() {
         return (
             <div>
                 <header> 
+                    <TopMenuButton/>
                     <div className="TopMenu_Container">
                         <div className="TopMenu_GridItem">
+                            <TopMenuButton/>
                         </div>
                         <div className="TopMenu_GridItem">
+                            <h2 className="TopMenu_h2">News</h2>
                         </div>
                         <div className="TopMenu_GridItem">
+                            <h2 className="TopMenu_h2">Skyland of Arnythos</h2>
                         </div>
                         <div className="TopMenu_GridItem">
+                            <h2 className="TopMenu_h2">Media</h2>
                         </div>
                         <div className="TopMenu_GridItem">
+                            <h2 className="TopMenu_h2">Contact</h2>
                         </div>
                         <div className="TopMenu_GridItem">
+                            <h2 className="TopMenu_h2">Team</h2>
                         </div>
                     </div>
                 </header>
