0caa3efc0132a86a06077159b6e5d8ab7a24d4ed
maximebl <maximebl@videotron.ca>
2017-04-26 23:35:18 -0400

    Started top menu using CSS grids

--- src/App.js
+++ src/App.js
@@ -4,11 +4,51 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MyAwesomeReactComponent from './components/MyAwesomeReactComponent';
+import TopMenu from './components/TopMenu/TopMenu';
 
 class App extends Component {
   render() {
     return (
         <div>
+          <TopMenu />
+          <div className="container">
+                        <div className="spring">
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/80625/sun.svg" alt="Sun" width="15%" />
+                        </div>
+                        <div className="spring">
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/80625/cloud_copy.svg" alt="Cloud" width="30%" />
+                        </div>
+                        <div className="spring">
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/80625/cloud_copy.svg" alt="Cloud" width="30%" />
+                        </div>
+                        <div className="spring"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/80625/rain.svg" alt="Rain" width="60%" /></div>
+                        <div className="spring">
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/80625/rain.svg" alt="Rain" width="60%" />
+                        </div>
+                        <div className="spring">
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/80625/rain.svg" alt="Rain" width="27%" />
+                        </div>
+                        <div className="spring">
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/80625/dragonfly.svg" alt="Dragonfly" width="30%" />
+                        </div>
+                        <div className="spring">
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/80625/butterfly.svg" alt="Butterfly" width="30%" />
+                        </div>
+                        <div className="spring">
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/80625/dragonfly.svg" alt="Dragonfly" width="30%" />
+                        </div>
+                        <div className="spring">
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/80625/butterfly.svg" alt="Butterfly" width="30%"/>
+                        </div>
+                        <div className="spring">
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/80625/flower1.svg" alt="First flower" />
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/80625/flower2.svg" alt="Second flower" />
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/80625/flower3.svg" alt="Third flower" />
+                        </div>
+                        <div className="spring">
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/80625/flowers2.svg" alt="Fourth flower" />
+                        </div>
+                    </div>
           <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
             <MyAwesomeReactComponent/>
           </MuiThemeProvider>
---
+++ src/components/TopMenu/TopMenu.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './TopMenu.css';
+
+class TopMenu extends React.Component {
+    render() {
+        return (
+            <div>
+                <header> 
+                    <div className="TopMenu_Container">
+                        <div className="TopMenu_GridItem">
+                        </div>
+                        <div className="TopMenu_GridItem">
+                        </div>
+                        <div className="TopMenu_GridItem">
+                        </div>
+                        <div className="TopMenu_GridItem">
+                        </div>
+                        <div className="TopMenu_GridItem">
+                        </div>
+                        <div className="TopMenu_GridItem">
+                        </div>
+                    </div>
+                </header>
+            </div>
+        );
+    }
+}
+export default TopMenu;
