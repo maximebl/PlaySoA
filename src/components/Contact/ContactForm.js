4f820daae72e7bab5fed431381961be4d0c63ed5
maximebl <maximebl@videotron.ca>
2017-05-02 14:25:03 -0400

    Working on the send button

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -9,15 +9,29 @@ import FlatButton from 'material-ui/FlatButton';
 import {grey900, lightGreen700, grey700} from 'material-ui/styles/colors'
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
-import RaisedButton from 'material-ui/RaisedButton';
-import IconButton from 'material-ui/IconButton';
-
+import FloatingActionButton from 'material-ui/FloatingActionButton';
 import HardwareVideogameAsset from 'material-ui/svg-icons/hardware/videogame-asset';
+import ContentSend from 'material-ui/svg-icons/content/send';
+import ContentUndo from 'material-ui/svg-icons/content/undo'
+
+const style = {
+  marginRight: 10,
+};
 
 class ContactForm extends Component {
     constructor(props) {
         super(props);
+        this.state = {buttonClicked:false};
+        this.sendButtonClickHandler = this.sendButtonClickHandler.bind(this);
+        
     }
+
+sendButtonClickHandler() {
+    this.setState({
+        buttonClicked:true
+    })
+    console.log(this.state.buttonClicked);
+}
 
     render(){
         return (
@@ -40,12 +54,16 @@ class ContactForm extends Component {
                                 floatingLabelText="Message"
                                 fullWidth={true}
                                 multiLine={true}/><br />
-                                <IconButton
-                                /*hoveredStyle={style={backgroundColor}}*/
-                                tooltip="Send"
-                                tooltipPosition="bottom-center">
-                                    <HardwareVideogameAsset/>
-                                </IconButton>
+                                <FloatingActionButton zDepth={1} className="CancelButton" backgroundColor={grey700} mini={true} style={style}>
+                                    <ContentUndo/>
+                                </FloatingActionButton>
+                                <FloatingActionButton zDepth={1} className="SendButton" backgroundColor={lightGreen700} iconClassName="SendButtonIcon" onClick={this.sendButtonClickHandler}>
+                                    {
+                                    this.state.buttonClicked
+                                        ? null
+                                        : <ContentSend hoverColor={lightGreen700} />
+                                    }
+                                </FloatingActionButton>
                             </div>
                         </Paper>
                     </div>
